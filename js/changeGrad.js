7b90b5980840dfbae55d59d2a19c99fb54706b12
Bruno <b.santos.slm@gmail.com>
2020-09-02 17:14:47 -0300

    First Commit

---
+++ js/changeGrad.js
@@ -0,0 +1,27 @@
+/** greeny **/
+document.getElementById ("btn1").addEventListener ("click", greeng, false);
+
+function greeng() {
+  document.getElementById('gradient').style.background = "green";
+}
+
+/** pinky**/
+document.getElementById ("btn2").addEventListener ("click", pinkyg, false);
+
+function pinkyg() {
+  document.getElementById('gradient').style.background = "pink";
+}
+
+/** bluey **/
+document.getElementById ("btn3").addEventListener ("click", blueyg, false);
+
+function blueyg() {
+  document.getElementById('gradient').style.background = "skyblue";
+}
+
+/** white noise**/
+document.getElementById ("btn4").addEventListener ("click", whiteg, false);
+
+function whiteg() {
+  document.getElementById('gradient').style.background = "white";
+}
